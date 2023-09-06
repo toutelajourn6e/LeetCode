f12229f47445aed90495ece39f5abbd61f4f992d
toutelajourn6e <toutelajourn6e@gmail.com>
2023-09-06 12:32:03 +0900

    Time: 36 ms (71.97%), Space: 16.4 MB (21.45%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.py
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.py
@@ -2,16 +2,11 @@ from string import ascii_uppercase
 
 class Solution:
     def convertToTitle(self, columnNumber: int) -> str:
-        chars = [0] + list(ascii_uppercase)
+        chars = list(ascii_uppercase)
         result = []
         
         while columnNumber:
-            columnNumber, digit = divmod(columnNumber, 26)
-            print(columnNumber, digit)
-            if not digit:
-                result.append('Z')
-                columnNumber -= 1
-            else:   
-                digit = chars[digit]
-                result.append(digit)
+            columnNumber, digit = divmod(columnNumber - 1, 26)
+            digit = chars[digit]
+            result.append(digit)
         return ''.join(result[::-1])
