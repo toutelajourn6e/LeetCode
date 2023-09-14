cc619148973c11929876827c24da919ece6c8a5e
toutelajourn6e <toutelajourn6e@gmail.com>
2023-09-14 13:38:18 +0900

    Time: 63 ms (11.11%), Space: 17.1 MB (13.04%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,15 +1,8 @@
-from collections import defaultdict
-
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        def check(string):
-            cnt = defaultdict(list)
-            for i in range(len(string)):
-                cnt[string[i]].append(i)
-            return cnt
-
-        for s_list, t_list in zip(check(s).values(), check(t).values()):
-            if len(s_list) != len(t_list): return False
-            for s_cnt, t_cnt in zip(s_list, t_list):
-                if s_cnt != t_cnt: return False
-        return True
+        map1, map2 = [], []
+        for ch in s:
+            map1.append(s.index(ch))
+        for ch in t:
+            map2.append(t.index(ch))
+        return True if map1 == map2 else False
