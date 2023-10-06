932ffbc9c8aaf4c2af8e5b6e895e0099e98582f0
toutelajourn6e <toutelajourn6e@gmail.com>
2023-10-06 16:55:20 +0900

    Time: 41 ms (31.97%), Space: 16.2 MB (87.06%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,14 +1,6 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         s = s.split()
-        if len(pattern) != len(s): return False
-        
-        patterns = {}
-        used = set()
-        for p, word in zip(pattern, s):
-            if p not in patterns and word not in used:
-                patterns[p] = word
-                used.add(word)
-            if p not in patterns or patterns[p] != word:
-                return False
-        return True
+        return (len(set(pattern)) ==
+                len(set(s)) ==
+                len(set(zip_longest(pattern,s))))
