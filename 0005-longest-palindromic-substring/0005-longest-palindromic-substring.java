c6080c427ca18c447a5d96c77aa39402cc54c398
toutelajourn6e <toutelajourn6e@gmail.com>
2024-02-13 19:25:40 +0900

    Time: 14 ms (92.37%), Space: 42.1 MB (79.75%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,38 +1,27 @@
 class Solution {
-    public String longestPalindrome(String s) {
-        int n = s.length();
-        int[] answer = {0, 1};
-        
-        for (int i = 0; i < n; i++) {
-            int start = i, end = i;
-            while (start >= 0 && end < n) {
-                if (s.charAt(start) == s.charAt(end)) {
-                    if ((answer[1] - answer[0]) < ((end+1) - start)) {
-                        answer[1] = end + 1;
-                        answer[0] = start;
-                    }
-                    start--;
-                    end++;
-                } else {
-                    break;
-                }
-            }
+    int[] answer = {0, 1};
 
-            start = i;
-            end = i + 1;
-            while (start >= 0 && end < n) {
-                if (s.charAt(start) == s.charAt(end)) {
-                    if ((answer[1] - answer[0]) < ((end+1) - start)) {
-                        answer[1] = end + 1;
-                        answer[0] = start;
-                    }
-                    start--;
-                    end++;
-                } else {
-                    break;
-                }
-            }
+    public String longestPalindrome(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            find(i ,i, s);
+            find(i, i + 1, s);
         }
         return s.substring(answer[0], answer[1]);
+    }
+
+    public void find(int start, int end, String s) {
+        int n = s.length();
+        while (start >= 0 && end < n) {
+            if (s.charAt(start) == s.charAt(end)) {
+                if ((answer[1] - answer[0]) < ((end+1) - start)) {
+                    answer[1] = end + 1;
+                    answer[0] = start;
+                }
+                start--;
+                end++;
+            } else {
+                break;
+            }
+        }
     }
 }
