9012a162d711155ab4741db8f8606cfb9b5901ea
toutelajourn6e <toutelajourn6e@gmail.com>
2024-02-17 16:37:19 +0900

    Time: 2602 ms (5%), Space: 53.1 MB (8.86%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> result = new HashSet<>();
+        Arrays.sort(nums);
+
+        
+        for (int i = 0; i < nums.length; i++) {
+            int firstSelected = nums[i];
+            int left = 0, right = nums.length - 1;
+
+            while (left < right) {
+                if (left == i) left++;
+                if (right == i) right--;
+                if (left == right || right >= nums.length) break;
+
+                if (firstSelected + nums[left] + nums[right] == 0) {
+                    List<Integer> arr = new ArrayList<>(Arrays.asList(firstSelected, nums[left], nums[right]));
+                    Collections.sort(arr);
+                    result.add(arr);
+                    left++;
+                } else if (firstSelected + nums[left] + nums[right] < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        List<List<Integer>> answer = new ArrayList<>(result);
+        return answer;
+    }
+}
