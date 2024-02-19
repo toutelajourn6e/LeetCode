cd968b83525b69534f08328bc86948fa225f387c
toutelajourn6e <toutelajourn6e@gmail.com>
2024-02-19 23:09:15 +0900

    Time: 14 ms (31.59%), Space: 46.5 MB (61.97%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        int sum = 0;
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i+=2) {
+            sum += Math.min(nums[i], nums[i+1]);
+        }
+
+        return sum;
+    }
+}
