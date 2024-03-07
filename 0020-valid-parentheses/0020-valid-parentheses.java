c3fee3a7e5cf41740d293a0c57b732735d6c961a
toutelajourn6e <toutelajourn6e@gmail.com>
2024-03-07 23:39:33 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -3,25 +3,19 @@ import java.util.*;
 class Solution {
     public boolean isValid(String s) {
         Deque<Character> stack = new ArrayDeque<>();
-        List<Character> close = new ArrayList(Arrays.asList(')', '}', ']'));
-
+        
         for (char c : s.toCharArray()) {
-            if (close.contains(c) && !stack.isEmpty()) {
-                if (c == ')' && stack.peek() == '(') {
-                    stack.pop();
-                } else if (c == '}' && stack.peek() == '{') {
-                    stack.pop();
-                } else if (c == ']' && stack.peek() == '[') {
-                    stack.pop();
-                } else {
-                    return false;
-                }
-            } else {
-                stack.push(c);
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();
             }
         }
 
-        if (stack.isEmpty()) return true;
-        else return false;
+        return stack.isEmpty();
     }
 }
